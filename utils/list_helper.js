f16f55fbfb237ff71948bc72db2f00c3941a0773
Deyvison Iury <iurydeyvison@gmail.com>
2024-07-31 16:32:33 -0300

    feat: add unit test for helper functions, exercises 4.3-4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -17,8 +17,62 @@ function favoriteBlog(blogs) {
   return favorite
 } 
 
+function mostBlogs(blogs) {
+  if(blogs.length === 0) return {};
+  if(blogs.length === 1) {
+    return {
+      author: blogs[0].author,
+      blogs: 1,
+    }
+  } 
+  
+  const authors = {};
+  const authorWithMostBlogs = {
+    author: "",
+    blogs: -1
+  };
+  blogs.forEach(blog => authors[blog.author] === undefined ? authors[blog.author] = 1 : authors[blog.author]++);
+
+  for(const [author, blogs] of Object.entries(authors)) {
+    if(authorWithMostBlogs.blogs < blogs) {
+      authorWithMostBlogs.author = author;
+      authorWithMostBlogs.blogs= blogs;
+    }
+  }
+
+  return authorWithMostBlogs
+}
+
+function mostLikes(blogs) {
+  if(blogs.length === 0) return {};
+  if(blogs.length === 1) {
+    return {
+      author: blogs[0].author,
+      likes: blogs[0].likes,
+    }
+  } 
+  
+  const authors = {};
+  const authorWithMostLikes = {
+    author: "",
+    likes: -1
+  };
+  blogs.forEach(blog => authors[blog.author] === undefined ? authors[blog.author] = blog.likes : authors[blog.author] += blog.likes);
+
+  for(const [author, likes] of Object.entries(authors)) {
+    if(authorWithMostLikes.likes < likes) {
+      authorWithMostLikes.author = author;
+      authorWithMostLikes.likes = likes;
+    }
+  }
+
+  return authorWithMostLikes
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
