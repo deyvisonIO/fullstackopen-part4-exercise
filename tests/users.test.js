4370abc25a6c9f0286bf68e1c56aa8aba3cf147e
Deyvison Iury <iurydeyvison@gmail.com>
2024-08-23 14:13:25 -0300

    Add users tests

---
+++ tests/users.test.js
@@ -0,0 +1,59 @@
+const { describe, test, beforeEach, after } = require("node:test");
+const assert = require("node:assert")
+const mongoose = require("mongoose")
+const { MONGODB_URL } = require("../utils/config") 
+const app = require("../app")
+const supertest = require("supertest")
+const User = require("../models/user") 
+const bcrypt = require("bcrypt") 
+
+const api = supertest(app);
+
+mongoose.connect(MONGODB_URL);
+
+
+
+describe("When there's initialy one user in the database", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("rootpassword", 10); 
+    const user = new User({ username: "root", passwordHash })
+
+    await user.save();
+  });
+
+  test("creation succeeds with a fresh username", async () => {
+    await api.post("/api/users").send({ username: "uniqueUsername", password: "uniquePassword" }).expect(201).expect("Content-Type", /application\/json/ );
+
+    const users = await User.find({});
+
+    assert.deepStrictEqual(users.length, 2);
+  });
+
+  test("creation fails with a username that already exists", async () => {
+    await api.post("/api/users").send({ username: "root", password: "uniquePassword" }).expect(409).expect("Content-Type", /application\/json/ );
+
+    const users = await User.find({});
+
+    assert.deepStrictEqual(users.length, 1);
+  })
+
+
+  test("creation fails if an username is not provided", async () => {
+    await api.post("/api/users").send({ password: "uniquePassword" }).expect(400).expect("Content-Type", /application\/json/ );
+  })
+
+
+  test("creation fails if a password is not provided", async () => {
+    await api.post("/api/users").send({ username: "test" }).expect(400).expect("Content-Type", /application\/json/ );
+  })
+
+  test("creation fails if username is less than 3 characters", async () => {
+    await api.post("/api/users").send({ username: "ro", password: "uniquePassword" }).expect(400).expect("Content-Type", /application\/json/ );
+  })
+
+  test("creation fails if password is less than 3 characters", async () => {
+    await api.post("/api/users").send({ username: "rootey", password: "ab" }).expect(400).expect("Content-Type", /application\/json/ );
+  })
+});
